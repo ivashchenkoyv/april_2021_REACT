075349831c129584a892f4a1c33a75f8a9b261e1
ivashchenkoyv <83450472+ivashchenkoyv@users.noreply.github.com>
2021-06-26 19:21:50 +0300

    hook useState

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,12 +1,30 @@
+import {useState} from "react";
+
 export default function Users(){
 
-    fetch()
+
+ let state= useState([]);
+ let userList=state[0]
+ let setUserList=state[1]
+
+
+
+// useEffect(()=>{
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+        .then(value => {
+            console.log(value)
+            setUserList(value);
+        })
+
+
 
 
     return(
 
         <div>
-           Here will be all Users
+            {userList.map(value => <div> dkjvnhtfjho</div>)}
         </div>
     )
 }
