8233cfe570f678db3e691011fd78758e8999d36e
ivashchenkoyv <83450472+ivashchenkoyv@users.noreply.github.com>
2021-06-27 17:47:52 +0300

    posts

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,14 +1,31 @@
+import {useEffect, useState} from "react";
+
 export default function Users(){
 
-fetch(https://jsonplaceholder.typicode.com/posts)
-    .then(value=>value.json());
-.then(value=>{
-    console.log(value)
-    })
+// let state=useState([])               //знизу записана спрощена аналогічна конструкція//
+//     let usersList=state[0]
+//     let setUsersList=state[1]
+
+let [usersList,setUsersList]=useState([])
+
+
+useEffect(()=>{
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(value=>value.json())
+        .then(response=>{
+            console.log(response);
+            setUsersList(response)
+        });
+
+
+},[])
+
 
     return(
         <div>
-            all Users will be here
+            {usersList.map(value => <div> {value.userId}-{value.title}</div>)
+                }
         </div>
     );
 
