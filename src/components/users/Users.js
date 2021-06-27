ddfc9c61aae659870779a9741de87b00ac4e2354
ivashchenkoyv <83450472+ivashchenkoyv@users.noreply.github.com>
2021-06-27 14:52:22 +0300

    fetch

---
+++ src/components/users/Users.js
@@ -0,0 +1,16 @@
+export default function Users(){
+
+fetch(https://jsonplaceholder.typicode.com/posts)
+    .then(value=>value.json());
+.then(value=>{
+    console.log(value)
+    })
+
+    return(
+        <div>
+            all Users will be here
+        </div>
+    );
+
+
+}
