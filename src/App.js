9b2d99772327df3500f675153f116cc0c28d49e5
ivashchenkoyv <83450472+ivashchenkoyv@users.noreply.github.com>
2021-06-28 19:00:08 +0300

    repair

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Users from "./components/users/Users";
+import Posts from "./components/posts/Posts";
 
 function App() {
   return (
     <div>
 <Users/>
+        <Posts/>
     </div>
   );
 }
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,1 +1,16 @@
 
+export default function Posts(){
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(value => value.json())
+    .then(response=>{
+        console.log(response)
+    })
+
+
+    return(<div>
+
+        here will be posts
+
+    </div>)
+}
