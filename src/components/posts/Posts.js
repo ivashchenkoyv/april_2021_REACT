55ef7f0e7d4476ffc2555c0b9183f8b5b86cd2d0
ivashchenkoyv <83450472+ivashchenkoyv@users.noreply.github.com>
2021-06-28 19:18:12 +0300

    with posts

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,16 +1,28 @@
+import {useEffect, useState} from "react";
 
 export default function Posts(){
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(value => value.json())
-    .then(response=>{
-        console.log(response)
-    })
+
+    let [postsList, setPostlist]=useState([])
+
+    useEffect(()=>{
+
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(value => value.json())
+            .then(response=>{
+                console.log(response)
+                setPostlist(response)
+            })
+    },[])
+
+
+
+
 
 
     return(<div>
 
-        here will be posts
+        {postsList.map(value=><div>{value.userId}-{value.title}</div>)}
 
     </div>)
 }
