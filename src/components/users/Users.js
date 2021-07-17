1dad0281f0a8347ffb191d866c3035df92e2eec8
ivashchenkoyv <83450472+ivashchenkoyv@users.noreply.github.com>
2021-07-17 16:29:07 +0300

    l3 початок

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,18 @@
+import {useEffect, useState} from "react";
+import {getUsers} from "../../services/Api";
 export default function Users() {
 
+ let [users,usersList]
+
+useEffect(()=>{getUsers().then(value => usersList(value.data))
+console.log(getUsers())
+
+
+},[]);
 
     return (
         <div>
-            Users
+            <Users items={users} />
 
         </div>
     );
