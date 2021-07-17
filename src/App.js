0f6b4a566af6e6358fdfde7b34699b9d8fa3516b
ivashchenkoyv <83450472+ivashchenkoyv@users.noreply.github.com>
2021-07-17 11:30:17 +0300

    l3 початок

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Link,Route} from 'react-router-dom'
+import Users from "./components/users/Users";
+import {getUsers} from './services/Api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+
+            <div>
+                <Link to={'/users'}>to users page</Link>
+                <br/>
+                <Link to={'/posts'}>to posts page</Link>
+                <br/>
+                <Link to={'/comments'}>to comments page</Link>
+
+                <Route path={'/users'} render={()=><getUsers/>}/>
+                <Route path={'/posts'} render={()=><div>{getUsers}</div>}/>
+                <Route path={'/comments'} render={()=><div>comments page</div>}/>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+// export default App;
--- src/services/Api.js
+++ src/services/Api.js
@@ -3,5 +3,7 @@ import axios from 'axios'
 let axiosInstance=axios.create({baseURL:'https://jsonplaceholder.typicode.com'});
 
 const getUsers=()=>axiosInstance('/users');
-const getPosts=()=>axiosInstance();
-const getComments=()=>axiosInstance()
+const getPosts=()=>axiosInstance('/posts');
+const getComments=()=>axiosInstance('/comments')
+
+export {getUsers,getPosts,getComments}
